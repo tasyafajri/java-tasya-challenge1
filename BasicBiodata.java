e1e861c2d295f5b5fe23a78da34c5ebac10780f9
Tasya Fajri Andia <tasya.fajri16@gmail.com>
2021-10-12 17:25:25 +0700

    first commit

---
+++ BasicBiodata.java
@@ -0,0 +1,32 @@
+
+public class BasicBiodata {
+    public static void main(String[] args) {
+        String firstname, lastname, address, hobi;
+        int age;
+        long phone;
+        double tinggi;
+        char GolonganDarah;
+
+
+    firstname = "Tasya";
+    lastname  = "Fajri";
+    address   = "Sidoarjo";
+    age       = 20;
+    phone     = +628384569;
+    tinggi    = 150;
+    GolonganDarah = 'B';
+    hobby      = "Nonton Film";
+
+
+    System.out.println("Nama                : " + firstName + lastName + "\n" +
+                        "Alamat             : " + address + "\n" +
+      					"Umur               : " + age + "\n" +
+                        "hobi               : " + hobby + "\n" +
+                        "Telepon            : " + phone + "\n" +
+                        "GolonganDarah      : " + GolonganDarah + "\n\n" + "Selesai");
+
+
+
+    }
+    
+}
